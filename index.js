9a4c7324b74c8411571bba06ab953499d18a08f7
subtain-ahmed <sibtiataj@gmail.com>
2024-12-16 03:23:01 +0500

    first commit

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ import express from "express";
 import connectDB from "./config/db.js";
 import submissionRoutes from "./routes/submissionRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import cors from 'cors';
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-}));
+app.use(
+  cors({
+    origin: "*" || "https://desol-fe-inky.vercel.app/",
+  })
+);
 
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
 connectDB();
 
-app.get('/', (req, res) => {
-    res.status(200).send("App is running.")
-})
+app.get("/", (req, res) => {
+  res.status(200).send("App is running.");
+});
 
 app.use("/", userRoutes);
 app.use("/", submissionRoutes);
